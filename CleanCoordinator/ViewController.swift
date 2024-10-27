915b89211343385410035f9c2c074fb61ad0bdeb
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-10-27 20:19:44 +0900

    [Setting] project 추가

---
+++ CleanCoordinator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanCoordinator
+//
+//  Created by Jisoo Ham on 10/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
